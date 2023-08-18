bd55fcc55bf17cbe180d8cdc5c18b1b1f82d6698
juliomunoz98 <julio-mu-oz98@hotmail.com>
2023-08-18 18:48:30 -0500

    controller de compras

---
+++ src/controllers/purchaseControllers.js
@@ -0,0 +1,77 @@
+const { Purchase, User, Orderdetail, Product } = require("../db");
+
+//para crear la orden hay que obtener el id de detaiOrders, sus precios y cantides. Para luego relacionar las DetailOrders con su respectiva order y obtener el totalPrice.
+const createPurchase = async (req, res, next) => {
+    const { orderIds, userId } = req.body;
+
+    console.log("este es el body:", req.body)
+    console.log(orderIds)
+    try {
+
+        // Verificar si ya existe una orden para los detalles proporcionados y el usuario
+        const existingPurchase = await Purchase.findOne({
+            where: {
+                userId: userId, // Filtrar por el ID del usuario
+            },
+            include: [
+                {
+                    model: Orderdetail,
+                    where: {
+                        id: orderIds, // Filtrar por los IDs de los detalles
+                    },
+                },
+            ],
+        });
+
+        if (existingPurchase) {
+            return res.status(409).json({
+                message: "There is a purchase created for all the orders_id given",
+            });
+        }
+        // Obtener los orders correspondientes a los IDs indicados
+        const ordersDetails = await Orderdetail.findAll({
+            where: {
+                id: orderIds, // Filtrar por los IDs indicados
+                userId: userId, // Filtrar por el ID del usuario
+            },
+            include: [Product], // Incluir el modelo Product si es necesario
+        });
+
+        if (ordersDetails.length === 0) {
+            return res.status(404).json({
+                message: "IDs and User requested are not found",
+            });
+        }
+
+        // Calcular el precio total de la compra sumando las multiplicaciones de cantidad y precio de las ordenes
+        let totalPurchasePrice = 0;
+        ordersDetails.forEach((orderdetail) => {
+            const totalPricePerItem = orderdetail.price * orderdetail.quantity;
+            totalPurchasePrice += totalPricePerItem;
+        });
+
+        // Crear la nueva compra
+        const newPurchase = await Purchase.create({
+            totalprice: totalPurchasePrice,
+            purchase_status: "in process",
+            userId: userId,
+            // Asignar el ID del usuario a la compra
+        });
+        // Asignar el ID del usuario a la orden
+        await newPurchase.setUser(userId);
+        await Promise.all(
+            ordersDetails.map((orderdetail) => orderdetail.setPurchase(newPurchase))
+        );
+        // Agregar el ID de la compra al usuario
+        const user = await User.findByPk(userId);
+        if (user) {
+            await user.addPurchase(newPurchase);
+        }
+        return res.status(200).json({ message: "Purchase created", purchase: newPurchase });
+    } catch (error) {
+        console.error("Purchase was not created", error);
+        return res.status(500).json({ message: "Purchase was not created" });
+    }
+};
+
+module.exports = { createPurchase };
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const router = Router();
 const productsRouter = require("./productsRouter");
 const ordersRouter = require("./ordersRouter");
 const usersRouter = require("./usersRouter.js");
+const purchaseRouter = require("./purchaseRouter");
 
 router.use(cors());
 router.use("/products", productsRouter);
 router.use("/orders", ordersRouter);
 router.use("/users", usersRouter);
+router.use("/purchases", purchaseRouter);
 
 module.exports = router;
